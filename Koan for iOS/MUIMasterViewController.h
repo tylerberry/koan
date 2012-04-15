40aa84cf3d723364f6301e069b68d0907fad7ae0
Tyler Berry <tyler@thoughtlocker.net>
2012-04-15 13:52:33 -0600

    Adding iOS target, stage 1.

---
+++ Koan for iOS/MUIMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MUIMasterViewController.h
+//  Koan for iOS
+//
+//  Created by Tyler Berry on 4/15/12.
+//  Copyright (c) 2012 3James Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MUIDetailViewController;
+
+@interface MUIMasterViewController : UITableViewController
+
+@property (strong, nonatomic) MUIDetailViewController *detailViewController;
+
+@end

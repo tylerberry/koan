00e80cdcf8d7ade1eb61335a5aea3ad37e7f1c67
Tyler Berry <tyler@thoughtlocker.net>
2013-01-07 02:26:22 -0700

    Added stub for Logging preference view controller.

---
+++ source/controllers/preferenceviewcontrollers/MULoggingPreferencesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MULoggingPreferencesViewController.h
+//  Koan
+//
+//  Created by Tyler Berry on 1/7/13.
+//  Copyright (c) 2013 3James Software. All rights reserved.
+//
+
+#import "MASPreferencesViewController.h"
+
+@interface MULoggingPreferencesViewController : NSViewController <MASPreferencesViewController>
+
+@end

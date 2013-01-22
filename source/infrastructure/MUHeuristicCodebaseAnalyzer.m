6c56158353774e4cf811ec43282f451a48e804fd
Tyler Berry <tyler@thoughtlocker.net>
2013-01-21 21:07:59 -0700

    Improvements to heuristics.

--- source/infrastructure/MUHeuristicCodebaseAnalyzer.m
+++ source/infrastructure/MUHeuristicCodebaseAnalyzer.m
@@ -43,6 +43,12 @@
   {
     NSArray *valueWords = [value componentsSeparatedByString: @" "];
     
+    if ([valueWords[0] isEqualToString: @"StickyMUSH"])
+    {
+      _definitiveCodebaseFound = YES;
+      self.codebase = MUCodebaseStickyMUSH;
+      self.codebaseFamily = MUCodebaseFamilyPennMUSH;
+    }
     if ([valueWords[0] isEqualToString: @"PennMUSH"])
     {
       _definitiveCodebaseFound = YES;
@@ -76,10 +82,49 @@
     return;
 }
 
-- (void) noteTextLine: (NSString *) text
+- (void) noteTextLine: (NSString *) textLine
 {
+  // TODO: Make this use regex instead, it's probably faster. Note we can't use NSRegularExpression because we run on
+  // 10.6.
+  
   if (_definitiveCodebaseFound)
     return;
+  
+  if (self.codebaseFamily == MUCodebaseFamilyUnknown)
+  {
+    // It's easy for people to talk about other codebases in chat, and we don't want to reset the heuristic every time
+    // somebody does because that would be awful. Therefore if we have any guess at all as to what we're running on, we
+    // shouldn't do textual matching anymore. We're hoping this is sent early, in the connect banner for example.
+    
+    if ([textLine.lowercaseString rangeOfString: @"pennmush"].location != NSNotFound)
+    {
+      self.codebase = MUCodebasePennMUSH;
+      self.codebaseFamily = MUCodebaseFamilyPennMUSH;
+    }
+    else if ([textLine.lowercaseString rangeOfString: @"mux"].location != NSNotFound)
+    {
+      self.codebase = MUCodebaseTinyMUX;
+      self.codebaseFamily = MUCodebaseFamilyTinyMUSH;
+    }
+    else if ([textLine.lowercaseString rangeOfString: @"dgd"].location != NSNotFound)
+    {
+      self.codebase = MUCodebaseLPMUDWithDGD;
+      self.codebaseFamily = MUCodebaseFamilyMUD;
+    }
+    else if ([textLine.lowercaseString rangeOfString: @"lpmud"].location != NSNotFound)
+    {
+      self.codebase = MUCodebaseLPMUD;
+      self.codebaseFamily = MUCodebaseFamilyMUD;
+    }
+    else if ([textLine.lowercaseString rangeOfString: @"mud"].location != NSNotFound)
+    {
+      self.codebaseFamily = MUCodebaseFamilyMUD;
+    }
+    else if ([textLine.lowercaseString rangeOfString: @"mush"].location != NSNotFound)
+    {
+      self.codebaseFamily = MUCodebaseFamilyTinyMUSH;
+    }
+  }
 }
 
 #pragma mark - Codebase-specific behavior tweaks
@@ -87,7 +132,7 @@
 - (BOOL) shouldSuppressGoAhead
 {
   // PennMUSH chokes when it receives IAC GA.
-  if (self.codebase == MUCodebasePennMUSH)
+  if (self.codebaseFamily == MUCodebaseFamilyPennMUSH)
     return YES;
   
   return NO;

c0b00f475950fbf591af14e0bcd6882d385dd1e7
Tyler Berry <tyler@3james.com>
2014-04-24 19:09:46 -0600

    Added comment explaining why a property is readwrite instead of readonly.

--- source/MUWorldRegistry.h
+++ source/MUWorldRegistry.h
@@ -8,7 +8,10 @@
 
 @interface MUWorldRegistry : NSObject
 
-@property (copy) NSMutableArray *worlds;
+@property (copy) NSMutableArray *worlds; // Needs to be readwrite for integration with MUProfilesSection.
+                                         //
+                                         // Could possibly be rewritten to have better encapsulation? But don't just
+                                         // change this without dealing with that as well.
 
 + (MUWorldRegistry *) defaultRegistry;
 

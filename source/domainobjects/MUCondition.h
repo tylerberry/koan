623836ad8c921617c5e4c3590014d92fba3e305d
Tyler Berry <tyler@3james.com>
2014-06-25 09:18:33 -0600

    First-pass implementation of MUCondition.

--- source/domainobjects/MUCondition.h
+++ source/domainobjects/MUCondition.h
@@ -4,8 +4,13 @@
 // Copyright (c) 2014 3James Software. All rights reserved.
 //
 
-@interface MUCondition : NSObject
+@interface MUCondition : NSObject <NSCopying, NSSecureCoding>
 
-@property (strong) NSPredicate *predicate;
+@property (copy) NSPredicate *predicate;
+@property (copy) NSMutableArray *actions;
+
+- (instancetype) initWithPredicate: (NSPredicate *) predicate actions: (NSArray *) actions;
+
+- (void) applyToMutableAttributedString: (NSMutableAttributedString *) mutableAttributedString;
 
 @end
--- source/domainobjects/MUCondition.m
+++ source/domainobjects/MUCondition.m
@@ -8,6 +8,52 @@
 
 @implementation MUCondition
 
+- (instancetype) initWithPredicate: (NSPredicate *) predicate actions: (NSArray *) actions
+{
+  if (!(self = [super init]))
+    return nil;
 
+  _predicate = [predicate copy];
+  _actions = [actions copy];
+
+  return self;
+}
+
+- (void) applyToMutableAttributedString: (NSMutableAttributedString *) mutableAttributedString
+{
+  if ([self.predicate evaluateWithObject: mutableAttributedString.string])
+    ;
+
+  return;
+}
+
+#pragma mark - NSCopying protocol
+
+- (id) copyWithZone: (NSZone *) zone
+{
+  return [[MUCondition alloc] initWithPredicate: self.predicate actions: self.actions];
+}
+
+#pragma mark - NSSecureCoding protocol
+
++ (BOOL) supportsSecureCoding
+{
+  return YES;
+}
+
+- (void) encodeWithCoder: (NSCoder *) coder
+{
+  [coder encodeObject: self.predicate forKey: @"predicate"];
+  [coder encodeObject: self.actions forKey: @"actions"];
+}
+
+- (instancetype) initWithCoder: (NSCoder *) decoder
+{
+  if (!(self = [self initWithPredicate: [decoder decodeObjectOfClass: [NSPredicate class] forKey: @"predicate"]
+                               actions: [decoder decodeObjectOfClass: [NSArray class] forKey: @"actions"]]))
+    return nil;
+
+  return self;
+}
 
 @end
